f69ca47f502e9ade648cc5645c510efee354972d
TT54 <tt54.of@gmail.com>
2026-01-31 21:28:31 +0100

    Correction bug sélection des armes randoms quand il n'y en a pas assez

--- src/main/java/fr/tt54/protectYourCastle/game/Game.java
+++ src/main/java/fr/tt54/protectYourCastle/game/Game.java
@@ -91,7 +91,7 @@ public class Game {
         if(GameParameters.ENABLE_RANDOM_WEAPONS.get()){
             List<Trader.GameWeapon> weapons = new ArrayList<>(Trader.weapons);
             Collections.shuffle(weapons);
-            this.selectedWeapons = weapons.subList(0, GameParameters.WEAPONS_TO_SELECT.get());
+            this.selectedWeapons = weapons.subList(0, Math.min(GameParameters.WEAPONS_TO_SELECT.get(), weapons.size()));
         }
 
         for(Player player : Bukkit.getOnlinePlayers()){
