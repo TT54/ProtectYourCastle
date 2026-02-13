e515a28c8740b14b952dad84b81d8bc3c6db4c6a
TT54 <tt54.of@gmail.com>
2026-02-13 19:16:15 +0100

    Ajout du chargement / déchargement du monde MC

--- src/main/java/fr/tt54/protectYourCastle/game/GameWorld.java
+++ src/main/java/fr/tt54/protectYourCastle/game/GameWorld.java
@@ -3,12 +3,18 @@ package fr.tt54.protectYourCastle.game;
 import com.google.common.reflect.TypeToken;
 import fr.tt54.protectYourCastle.ProtectYourCastleMain;
 import fr.tt54.protectYourCastle.utils.FileManager;
+import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.WorldCreator;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Stream;
 
 public class GameWorld {
 
@@ -18,33 +24,46 @@ public class GameWorld {
     public static final Type weaponsType = new TypeToken<List<Trader.GameWeapon>>() {}.getType();
 
 
-    private final String name;
+    private final String worldName;
     private final Map<Team.TeamColor, Team> loadedTeams = new HashMap<>();
     private final Map<UUID, Trader> traders = new HashMap<>();
     private final List<Trader.GameWeapon> weapons = new ArrayList<>();
     private final List<ResourceGenerator> resourceGenerators = new ArrayList<>();
     private World world;
 
-    public GameWorld(String name) {
-        this.name = name;
+    public GameWorld(String worldName) {
+        this.worldName = worldName;
     }
 
-    public void load(){
+    /**
+     * Charge le monde de jeu
+     * @return true si le monde a correctement été chargé, false sinon
+     */
+    public boolean load(){
         this.loadTeams();
         this.loadTraders();
         this.loadResourceGenerators();
+        this.world = this.loadWorld();
+        return this.world != null;
     }
 
-    public void save(){
+    public void saveData(){
         this.saveTeams();
         this.saveTraders();
         this.saveResourceGenerators();
     }
 
+    public void unload(boolean save){
+        this.unloadWorld(save);
+        if(save) {
+            this.saveData();
+        }
+    }
+
     private File loadFile(@NotNull String fileName){
-        File teamsFile = FileManager.getFileWithoutCreating("worlds/" + this.name + "/" + fileName, ProtectYourCastleMain.getInstance());
+        File teamsFile = FileManager.getFileWithoutCreating("worlds/" + this.worldName + "/" + fileName, ProtectYourCastleMain.getInstance());
         if (!teamsFile.exists()) {
-            FileManager.saveResource("teams.json", "worlds/" + this.name + "/" + fileName, ProtectYourCastleMain.getInstance());
+            FileManager.saveResource("teams.json", "worlds/" + this.worldName + "/" + fileName, ProtectYourCastleMain.getInstance());
         }
         return teamsFile;
     }
@@ -80,5 +99,49 @@ public class GameWorld {
 
     private void saveResourceGenerators(){
         FileManager.write(Game.gson.toJson(this.resourceGenerators), loadFile("generators.json"));
+    }
+
+    private World loadWorld(){
+        File sourceGameWorldFolder = new File(ProtectYourCastleMain.getInstance().getDataFolder(), "worlds/" + this.worldName);
+        File gameWorldFolder = new File(ProtectYourCastleMain.getInstance().getDataFolder().getParentFile().getParentFile(), this.worldName);
+        if(gameWorldFolder.exists()) {
+            try (Stream<Path> paths = Files.walk(gameWorldFolder.toPath())) {
+                paths.sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if(!sourceGameWorldFolder.exists() || !sourceGameWorldFolder.isDirectory()){
+            return null;
+        }
+
+        WorldCreator creator = new WorldCreator(this.worldName);
+        return creator.createWorld();
+    }
+
+    private void unloadWorld(boolean save) {
+        if(this.world == null){
+            System.err.println("Le monde " + this.worldName + " a tenté d'être déchargé alors qu'il n'était pas chargé");
+            return;
+        }
+
+        Bukkit.unloadWorld(this.world, save);
+        this.world = null;
+
+        if(save) {
+            File sourceGameWorldFolder = new File(ProtectYourCastleMain.getInstance().getDataFolder(), "worlds/" + this.worldName);
+            File gameWorldFolder = new File(ProtectYourCastleMain.getInstance().getDataFolder().getParentFile().getParentFile(), this.worldName);
+
+            if(sourceGameWorldFolder.exists()) {
+                try (Stream<Path> paths = Files.walk(sourceGameWorldFolder.toPath())) {
+                    paths.sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            FileManager.copy(gameWorldFolder, sourceGameWorldFolder);
+        }
+        System.out.println("Monde " + this.world.getName() + " déchargé !");
     }
 }
